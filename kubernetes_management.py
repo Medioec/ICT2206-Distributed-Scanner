d9c653eef27db99c7a54efe0449ad37347db8f7b
Medioec <2103020@sit.singaporetech.edu.sg>
2023-03-02 20:51:43 +0800

    small fix

--- kubernetes_management.py
+++ kubernetes_management.py
@@ -3,5 +3,5 @@ from models.vm import VM
 
 def delete_node(vm: VM):
     node_name = vm.hostname.lower()
-    subprocess.run(f"sudo kubectl delete node {node_name}")
+    subprocess.run(f"sudo kubectl delete node {node_name}".split())
     print(f"Deleted node {node_name} from k3s")
